d74a2a7667f5301b66e2905d302874482e6b9e73
Jakob Asgaard <jasninus8@hotmail.dk>
2019-12-14 18:49:38 +0000

    Sample code

---
+++ ExorcistHealth.cs
@@ -0,0 +1,75 @@
+﻿public class ExorcistHealth : MonoBehaviour, IDamageable
+{
+    [SerializeField] private int maxHealth;
+    private int health;
+
+    [SerializeField] private float invulnerabilityTime;
+    private float lastDamageTime;
+
+    [SerializeField] private Image healthBar;
+
+    [HideInInspector] public bool immune;
+
+    [SerializeField] private Animator anim;
+
+    private AudioSource audio;
+
+    [SerializeField] private AudioClip hitSFX;
+
+    private void Awake()
+    {
+        audio = GetComponent<AudioSource>();
+        health = maxHealth;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        if (immune)
+        {
+            return;
+        }
+
+        if (Time.time > lastDamageTime + invulnerabilityTime)
+        {
+            ApplyDamage(damage);
+            CheckForGameOver();
+        }
+    }
+
+    private void ApplyDamage(int damage)
+    {
+        audio.clip = hitSFX;
+        audio.Play();
+        anim.SetBool("Damage", true);
+        lastDamageTime = Time.time;
+        health -= damage;
+
+        healthBar.fillAmount = (float)health / maxHealth;
+    }
+
+    private void CheckForGameOver()
+    {
+        if (health <= 0)
+        {
+            GameOver();
+        }
+    }
+
+    private void OnCollisionStay2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "UnholyWater" && !immune)
+        {
+            GameOver();
+        }
+    }
+
+    private void GameOver()
+    {
+        SceneManager.LoadScene(4);
+    }
+
+    public void StopTakingDamage()
+    {
+        anim.SetBool("Damage", false);
+    }
+}
---
+++ HolyHandGrenade.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class HolyHandGrenade : MonoBehaviour
+{
+    [SerializeField] private int damage;
+
+    [SerializeField] private float fuseTime, range;
+    private float startTime;
+
+    private bool hasExploded;
+
+    private ParticleSystem particle;
+
+    private AudioSource audio;
+
+    private void Awake()
+    {
+        audio = GetComponent<AudioSource>();
+        particle = GetComponent<ParticleSystem>();
+    }
+
+    private void Start()
+    {
+        startTime = Time.time;
+    }
+
+    private void Update()
+    {
+        if (Time.time > startTime + fuseTime && !hasExploded)
+        {
+            particle.Play(true);
+            audio.Play();
+            hasExploded = true;
+            IDamageable[] hit = Physics2D.OverlapCircleAll(transform.position, range).Select(c => c.transform.GetComponent<IDamageable>()).ToArray();
+
+            foreach (IDamageable damageable in hit)
+            {
+                damageable?.TakeDamage(damage);
+            }
+
+            Debug.Log("BOOM! Holyness overcomes you");
+            GetComponent<SpriteRenderer>().enabled = false;
+            StartCoroutine(Destroy(particle.main.duration));
+        }
+    }
+
+    private IEnumerator Destroy(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        Destroy(gameObject);
+    }
+}
